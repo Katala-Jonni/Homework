0e112463862afc718e87121f7dfb730ac3dc5ca6
Katala-Jonni <katala.jonni@yandex.ru>
2018-03-15 23:49:56 +0300

    Решение

--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -1,31 +1,50 @@
 'use strict';
 
 function showComments(list) {
-  const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+
+    const commentsContainer = document.querySelector('.comments');
+    const comments = list.map(createComment) //.join('');
+    const fragment = comments.reduce((frag, elem) => {
+        frag.appendChild(elem);
+        return frag;
+    }, document.createDocumentFragment());
+
+    commentsContainer.appendChild(fragment);
+    commentsContainer.style.whiteSpace = 'pre-line';
+}
+
+function el(tag, attr, child) {
+
+    const elem = document.createElement(tag);
+    if (typeof attr === 'object') {
+        Object.keys(attr).forEach(el => elem.setAttribute(el, attr[el]));
+    }
+    if (typeof child === 'string' || typeof child === 'number') {
+        elem.textContent = child;
+    } else if (child instanceof Array) {
+        child.forEach(el => elem.appendChild(el));
+    }
+    return elem;
 }
 
 function createComment(comment) {
-  return `<div class="comment-wrap">
-    <div class="photo" title="${comment.author.name}">
-      <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
-    </div>
-    <div class="comment-block">
-      <p class="comment-text">
-        ${comment.text.split('\n').join('<br>')}
-      </p>
-      <div class="bottom-comment">
-        <div class="comment-date">${new Date(comment.date).toLocaleString('ru-Ru')}</div>
-        <ul class="comment-actions">
-          <li class="complain">Пожаловаться</li>
-          <li class="reply">Ответить</li>
-        </ul>
-      </div>
-    </div>
-  </div>`
+    return el('div', { class: 'comment-wrap' }, [
+        el('div', { class: 'photo', title: comment.author.name }, [
+            el('div', { class: 'avatar', style: `background-image: url(${comment.author.pic})` })
+        ]),
+        el('div', { class: 'comment-block' }, [
+            el('p', { class: 'comment-text' }, `${comment.text}`),
+            el('div', { class: 'bottom-comment' }, [
+                el('div', { class: 'comment-date' }, `${new Date(comment.date).toLocaleString('ru-Ru')}`), ///
+                el('ul', { class: 'comment-actions' }, [
+                    el('li', { class: 'complain' }, `Пожаловаться`),
+                    el('li', { class: 'reply' }, `Ответить`)
+                ])
+            ])
+        ])
+    ]);
 }
 
 fetch('https://neto-api.herokuapp.com/comments')
-  .then(res => res.json())
-  .then(showComments);
+    .then(res => res.json())
+    .then(showComments);
--- dom-from-zero/plane/js/scheme.js
+++ dom-from-zero/plane/js/scheme.js
@@ -1,1 +1,32 @@
-'use strict'
+'use strict';
+// buttons
+const btnSeatMap = document.querySelector('#btnSeatMap');
+const btnSetFull = document.querySelector('#btnSetFull');
+const btnSetEmpty = document.querySelector('#btnSetEmpty');
+const plane = document.querySelector('#acSelect');
+const seatMapTitle = document.querySelector('#seatMapTitle');
+const option = plane.querySelectorAll('option');
+
+btnSeatMap.addEventListener('click', onViewPlane);
+
+function onViewPlane() {
+    event.preventDefault();
+    //seatMapTitle.textContent = ' ';
+    const search = Array.from(option).find(el => el.value === plane.value);
+      console.log(search)
+          seatMapTitle.textContent = search.textContent
+    fetch(`https://neto-api.herokuapp.com/plane/${plane.value}`)
+        .then(res => res.json())
+        .then(getPlaces)
+}
+
+function getPlaces(place) {
+
+
+    
+
+    //seatMapTitle.textContent
+    //	plane.querySelector('option')
+    console.log(place);
+
+}
